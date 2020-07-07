6c12a6fb3637b91f6b4c288cb391bcec943aff1d
koro <koro@ganguo.hk>
2020-07-07 20:35:28 +0800

    add facade

--- src/WeatherFacade.php
+++ src/WeatherFacade.php
@@ -5,3 +5,14 @@
  * Date: 2019-10-16
  * Time: 下午 03:01
  */
+
+namespace Hym510\Weather;
+
+use Illuminate\Support\Facades\Facade;
+
+class WeatherFacade extends Facade
+{
+    protected static function getFacadeAccessor() {
+        return 'weather';
+    }
+}
